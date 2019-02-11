ff589252ef1af63fa2c4fc6566af6ee466d02f29
Gopinath Guptha <gopinath.br@gmail.com>
2019-02-11 20:47:03 +0530

    Fixed an issue with oAuth login redirect ...

--- risk-rating-model/src/main/java/com/pfs/riskmodel/controller/LoanApplicationController.java
+++ risk-rating-model/src/main/java/com/pfs/riskmodel/controller/LoanApplicationController.java
@@ -6,10 +6,13 @@ import com.pfs.riskmodel.resource.LoanApplicationResource;
 import com.pfs.riskmodel.resource.SearchResource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 import java.util.List;
 
 @ApiController
@@ -23,10 +26,15 @@ public class LoanApplicationController {
         SearchResource resource = new SearchResource();
         if (!projectName.equals("undefined"))
             resource.setPartyName(projectName);
-        ResponseEntity<List<LoanApplicationResource>> resources = lmsEnquiryClient.searchEnquiries(resource);
+        ResponseEntity<List<LoanApplicationResource>> resources = lmsEnquiryClient.searchEnquiries(resource,getAuthorizationBearer(request.getUserPrincipal()));
 
         // ResponseEntity<List<LoanApplicationResource>> resources = lmsEnquiryClient.searchEnquiries(resource,"Basic YWRtaW46YWRtaW4");
 
         return ResponseEntity.ok(resources);
+    }
+
+    public String getAuthorizationBearer(Principal user) {
+        OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) ((OAuth2Authentication) user).getDetails();
+        return "Bearer " + details.getTokenValue();
     }
 }
