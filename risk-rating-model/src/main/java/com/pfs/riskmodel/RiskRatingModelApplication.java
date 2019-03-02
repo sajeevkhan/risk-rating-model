03fe1778b2dd770164236a976f3cb290c56bf65d
sajeevkhan <sajeev.khan@gmail.com>
2019-03-02 18:55:24 +0100

    Get User by Email PDF Fixes Set Reviewer and Creator

--- risk-rating-model/src/main/java/com/pfs/riskmodel/RiskRatingModelApplication.java
+++ risk-rating-model/src/main/java/com/pfs/riskmodel/RiskRatingModelApplication.java
@@ -2,6 +2,7 @@ package com.pfs.riskmodel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,13 +10,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-@ComponentScan({"com.pfs","com.pfs.riskmodel.businessconfig",
-				"com.pfs.riskmodel",
-		         "com.pfs.riskmodel.repository",
-		          "com.pfs.riskmodel.service", "com.pfs.riskmodel.pdfservice"})
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableFeignClients
+
 public class RiskRatingModelApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
