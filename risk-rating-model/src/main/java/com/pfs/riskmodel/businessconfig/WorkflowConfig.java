c0ee2a89bbd3946cb501e3c3fef34cb0140aa275
sajeevkhan <sajeev.khan@gmail.com>
2019-03-25 17:42:11 +0530

    PFS workflow assignment corrections again....

--- risk-rating-model/src/main/java/com/pfs/riskmodel/businessconfig/WorkflowConfig.java
+++ risk-rating-model/src/main/java/com/pfs/riskmodel/businessconfig/WorkflowConfig.java
@@ -59,6 +59,9 @@ public class WorkflowConfig implements CommandLineRunner{
             if (p1 == null)
                 p1 = new RiskPurpose(null, "01", "Project");
 
+            System.out.println("Risk Purpose p1 : " + p1.toString());
+
+
 //            RiskPurpose p2 = riskPurposeRepository.findByCode("02");
 //            if (p2 == null) {
 //                p2 = new RiskPurpose(null, "02", "Risk");
@@ -67,23 +70,34 @@ public class WorkflowConfig implements CommandLineRunner{
             if (p3 == null) {
                 p3 = new RiskPurpose(null, "03", "Monitoring");
             }
-//            RiskPurpose p4 = riskPurposeRepository.findByCode("04");
+
+            System.out.println("Risk Purpose p3 : " + p3.toString());
+
+
+        //
+//
+// RiskPurpose p4 = riskPurposeRepository.findByCode("04");
 //            if (p4 == null) {
 //                p4 = new RiskPurpose(null, "04", "IT Department");
 //            }
 
-        WorkflowAssignment wa1 = new WorkflowAssignment();
+            WorkflowAssignment wa1 = new WorkflowAssignment();
             WorkflowAssignment wa2 = new WorkflowAssignment();;
-            WorkflowAssignment wa3 = new WorkflowAssignment();;
+    //        WorkflowAssignment wa3 = new WorkflowAssignment();;
 
 
             wa1 = workflowAssignmentRepository.findByPurpose(p1);
+            if (wa1 != null)
+                System.out.println("Workflow Assignment wa1: " + wa1.toString());
+
             if (wa1 == null) {
+                wa1.setPurpose(p1);
                 wa1 = new WorkflowAssignment(null, p1, "Sitesh Kumar Sinha", "sksinha@ptcfinancial.com",
                         "Neeraj Yadav", "neerajyadav@ptcfinancial.com",
                         "Devesh Singh", "devesh@ptcfinancial.com");
 
             }else {
+                wa1.setPurpose(p1);
                 wa1.setFirstLevelApproverEmailId("sksinha@ptcfinancial.com");
                 wa1.setFirstLevelApproverName("Sitesh Kumar Sinha");
                 wa1.setSecondLevelApproverEmailId("neerajyadav@ptcfinancial.com");
@@ -92,15 +106,21 @@ public class WorkflowConfig implements CommandLineRunner{
                 wa1.setThirdLevelApproverName("Devesh Singh");
             }
 
-            workflowAssignmentRepository.save(wa1);
+        System.out.println("Workflow Assignment wa1 BEFORE SAVE : " + wa1.toString());
 
-            wa2 = workflowAssignmentRepository.findByPurpose(p3);
+        workflowAssignmentRepository.save(wa1);
+        if (wa2 != null)
+            System.out.println("Workflow Assignment wa2: " + wa2.toString());
+
+        wa2 = workflowAssignmentRepository.findByPurpose(p3);
             if (wa2 == null) {
+                wa2.setPurpose(p3);
                 wa2 = new WorkflowAssignment(null, p3, "Sanjay Rustagi", "sksinha@ptcfinancial.com",
                         "Neeraj Yadav", "neerajyadav@ptcfinancial.com",
                         "Devesh Singh", "devesh@ptcfinancial.com");
 
             }else {
+                wa2.setPurpose(p3);
                 wa2.setFirstLevelApproverEmailId("sanjay.rustagi@ptcfinancial.com");
                 wa2.setFirstLevelApproverName("Sanjay Rustagi");
                 wa2.setSecondLevelApproverEmailId("neerajyadav@ptcfinancial.com");
@@ -109,7 +129,9 @@ public class WorkflowConfig implements CommandLineRunner{
                 wa2.setThirdLevelApproverName("Devesh Singh");
             }
 
-            workflowAssignmentRepository.save(wa2);
+        System.out.println("Workflow Assignment wa2BEFORE SAVE : " + wa2.toString());
+
+        workflowAssignmentRepository.save(wa2);
 
 
 
