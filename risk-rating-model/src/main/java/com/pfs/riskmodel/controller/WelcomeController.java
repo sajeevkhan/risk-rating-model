25c8bd6ceef6dc0bab11b2d4fa1ba5eb561483a4
Sajeev <sajeev.khan@gmail.com>
2020-08-13 09:27:42 +0530

    Welcome Controller Debug

--- risk-rating-model/src/main/java/com/pfs/riskmodel/controller/WelcomeController.java
+++ risk-rating-model/src/main/java/com/pfs/riskmodel/controller/WelcomeController.java
@@ -4,12 +4,13 @@ import com.pfs.riskmodel.config.ApiController;
 import com.pfs.riskmodel.resource.User;
 import com.pfs.riskmodel.service.IWelcomeService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.logging.Logger;
-
+@Slf4j
 @ApiController
 @RequiredArgsConstructor
 @Profile("!localdev")
@@ -19,6 +20,8 @@ public class WelcomeController {
 
     @GetMapping("/welcome")
     public ResponseEntity<User> welcome() {
+
+        log.info("Welcome Controller.........  Getting User");
         User user = welcomeService.getUser();
 
         return ResponseEntity.ok(user);
